8de1bd24faae960e28c7b7060bc0d149ac3de64a
Johnny Webber <jwebber@il-d6.21-school.ru>
2019-10-26 14:37:20 +0300

    Fixed fill del

--- ft_fill_del.c
+++ ft_fill_del.c
@@ -1,10 +1,19 @@
-//
-// Created by reoil on 14.10.2019.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fill_del.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/26 14:36:44 by jwebber           #+#    #+#             */
+/*   Updated: 2019/10/26 14:37:05 by jwebber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "fillit.h"
 
-void ft_fill_del(void *data, size_t size) {
-    (void) size;
-    ft_memdel(&data);
+void	ft_fill_del(void *data, size_t size)
+{
+	(void)size;
+	ft_memdel(&data);
 }
