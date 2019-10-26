378f250237d894c6111292bf656c74134cd4f942
Johnny Webber <jwebber@il-d6.21-school.ru>
2019-10-26 14:30:00 +0300

    Norminate main

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/26 14:28:51 by jwebber           #+#    #+#             */
+/*   Updated: 2019/10/26 14:29:48 by jwebber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
-int main(int argc, char **argv) {
-    t_list *lst;
-    if (argc != 2) {
-        ft_putendl("usage: fillit file");
-        return (0);
-    }
-    lst = ft_parse(argv[1]);
-    if (!lst)
-        ft_error(1);
-    ft_brute(lst);
-    ft_lstdel(&lst, ft_fill_del);
-    return 0;
+int	main(int argc, char **argv)
+{
+	t_list *lst;
+
+	if (argc != 2)
+	{
+		ft_putendl("usage: fillit file");
+		return (0);
+	}
+	lst = ft_parse(argv[1]);
+	if (!lst)
+		ft_error(1);
+	ft_brute(lst);
+	ft_lstdel(&lst, ft_fill_del);
+	return (0);
 }
