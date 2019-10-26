3b847e3e2b5080a2962e2577b0ac4fbb732d3f66
Johnny Webber <jwebber@il-d6.21-school.ru>
2019-10-26 14:38:41 +0300

    Norm fix

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,11 +1,20 @@
-//
-// Created by reoil on 14.10.2019.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/26 14:37:41 by jwebber           #+#    #+#             */
+/*   Updated: 2019/10/26 14:38:13 by jwebber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "fillit.h"
 
-size_t ft_lstsize(t_list *list) {
-    if (list == NULL)
-        return (0);
-    return ft_lstsize(list->next) + 1;
+size_t	ft_lstsize(t_list *list)
+{
+	if (list == NULL)
+		return (0);
+	return (ft_lstsize(list->next) + 1);
 }
